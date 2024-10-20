3765a6650f45c9fbb76644c66c318ca369846c22
DevilsFruit <Jiaheng@live.nl>
2024-10-20 18:12:51 +0200

    fixed issues with code, and added a method to split the asteroids

--- asteroidfield.py
+++ asteroidfield.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-from asteroid import *
+from asteroid import Asteroid
 from constants import *
 
 
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import pygame
 import sys
+import pygame
 from constants import *
-from player import *
-from asteroid import *
-from asteroidfield import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+
 
 def main():
     pygame.init()
@@ -15,33 +17,45 @@ def main():
     asteroids = pygame.sprite.Group()
     shots = pygame.sprite.Group()
 
-    Player.containers = (updatable, drawable)
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
     Asteroid.containers = (asteroids, updatable, drawable)
     Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
 
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        dt = clock.tick(60) / 1000  
-        updatable.update(dt)
 
-        for entity in updatable:
-            entity.update(dt)
-        
-        screen.fill("black")
-        for entity in drawable:
-            entity.draw(screen)
-        pygame.display.flip()
+        for obj in updatable:
+            obj.update(dt)
 
         for asteroid in asteroids:
-            if player.is_colliding_with(asteroid):
+            if asteroid.collides_with(player):
                 print("Game over!")
                 sys.exit()
-    
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+
 if __name__ == "__main__":
     main()
